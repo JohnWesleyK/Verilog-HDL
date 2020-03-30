2c9cf74448897d40daae19914d7815a0a9d0a127
JohnWesleyK <59068112+JohnWesleyK@users.noreply.github.com>
2020-03-30 18:24:14 +0100

    Create SerialComplementer.v
    Assignment 10 DLD

---
+++ SerialComplementer.v
@@ -0,0 +1,19 @@
+
+module Serial_Twos_Comp (output y, input [7: 0] data, input load, shift_control, Clock, reset_b); 
+  reg [7: 0] SReg; 
+  reg Q;
+  wire SO = SReg [0];
+  assign y = SO ^ Q;
+  always @ (posedge Clock, negedge reset_b) 
+  if (reset_b == 0) begin
+    SReg <= 0; 
+    Q <= 0; 
+  end 
+  else begin
+    if (load) SReg = data;
+   else if (shift_control) begin 
+      Q <= Q | SO;
+     SReg <= {y, SReg[7: 1]}; 
+    end 
+  end 
+endmodule  
